65860e12846acb6890f15bcb6c6637bd37f7be94
kelvinwelter <37090245+kelvinwelter@users.noreply.github.com>
2018-12-03 10:27:33 -0200

    Update smart_buttom.ino

--- smart_buttom.ino
+++ smart_buttom.ino
@@ -34,7 +34,9 @@ MyNanofox.Init_Modem_WISOL(RC2);  //Inicialização do WISOL Sigfox Modem
 Serial.println("Welcome to NANOFOX IoT Kit!");
 Serial.println("Smartbuttom com Nanofox IoT");
 Serial.println("Pressione o botão por 2 segundos para ativar");
-pinMode(5, INPUT_PULLUP); //Definindo o pino D2 como input com pullup interno
+pinMode(3, INPUT_PULLUP); //Definindo o pino D2 como input com pullup interno
+
+Uplink_Buffer[0] = 01;
 
 timeref_ms = millis();  // Inicialização da referência de tempo
 }
@@ -42,17 +44,14 @@ timeref_ms = millis();  // Inicialização da referência de tempo
 void loop() {
 
   delay(10);
-
-  Serial.println(digitalRead(5));
-  delay(3000);
   
-  while(digitalRead(5) == LOW){ //Leitura do pino D2 e estrutura de repetição while para caso o botão seja pressionado
+  while(digitalRead(3) == LOW){ //Leitura do pino D2 e estrutura de repetição while para caso o botão seja pressionado
     
   delay(2000); //Aguardo de 2 segundos para conferir novamente se o botão está pressionado
 
-    if(digitalRead(5) == LOW){ //Se o botão estiver pressionado após os dois segundos, a placa Nanofox IoT transmite sinal 
+    if(digitalRead(3) == LOW){ //Se o botão estiver pressionado após os dois segundos, a placa Nanofox IoT transmite sinal 
       Serial.println("Botão pressionado");  
-      MyNanofox.Send_Payload_Sigfox(&Uplink_Buffer[0],2,&Downlink_Buffer[0],0);
+      MyNanofox.Send_Payload_Sigfox(&Uplink_Buffer[0],1,&Downlink_Buffer[0],0);
     }
   }
 }
